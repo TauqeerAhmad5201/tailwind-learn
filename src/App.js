1d8b8564aa9e87845435a0f596d59b561be1053e
TauqeerAhmad5201 <hellotauqeer@gmail.com>
2023-03-07 11:04:47 +0530

    corrected content in tailwind.config.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div>
-    <h1 className="text-5xl font-bold">
+    <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
     <p class="text-4xl">The quick brown fox ...</p>
