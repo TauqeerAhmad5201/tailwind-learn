c8cc1a5729251074a12bccf80e9dac855049bfc3
TauqeerAhmad5201 <hellotauqeer@gmail.com>
2023-03-07 22:13:14 +0530

    added base code CTA

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -143,7 +143,7 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
 
-export default function Example() {
+export default function Navbar() {
   const [open, setOpen] = useState(false)
 
   return (
