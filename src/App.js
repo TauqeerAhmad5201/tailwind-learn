144b4f267264302d90453a58757ca8a9fc8afc37
TauqeerAhmad5201 <hellotauqeer@gmail.com>
2023-03-07 18:24:47 +0530

    added navbar problem

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
+import Navbar from './components/Navbar'
 
 function App() {
   return (
-    <div>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    <p class="text-4xl">The quick brown fox ...</p>
-    </div>
+    <>
+    <Navbar></Navbar>
+    </>
   );
 }
 
